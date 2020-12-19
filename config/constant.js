37eed0fb4d524babff77dff2a7b8fe655f9c52af
chuyenpn <chuyenpn@rikkeisoft.com>
2020-12-19 10:53:53 +0700

    add connect wallet

--- config/constant.js
+++ config/constant.js
@@ -1,4 +1,5 @@
 module.exports =  {
     URL: `https://lcd.orai.io`,
-    TRY_URL: "https://api.market.orai.io"
+    TRY_URL: "https://api.market.orai.io",
+    WALLET_API_URL: "http://localhost:8080"
 }
